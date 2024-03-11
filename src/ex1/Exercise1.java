321646ac576b845aff2cf62abb03d01e6451432f
Antonio Ruggia Piquer <149959739+kaem0n@users.noreply.github.com>
2024-03-11 14:45:16 +0100

    project start

---
+++ src/ex1/Exercise1.java
@@ -0,0 +1,11 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Scanner;
+
+public class Main {
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
---
+++ src/ex2/Exercise2.java
@@ -0,0 +1,13 @@
+package ex1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Scanner;
+
+public class Exercise1 {
+    private static Logger logger = LoggerFactory.getLogger(Exercise1.class);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
